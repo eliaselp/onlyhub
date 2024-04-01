c6861f2b34fb451653e38bdd2f232db95374ef41
Elias Eduardo Liranza Pérez <146482192+eliaselp@users.noreply.github.com>
2024-04-01 00:37:26 -0700

    Update settings.py
    Cambiando la configuracion de la conexion a la base de datos

--- LinkWolf/settings.py
+++ LinkWolf/settings.py
@@ -78,23 +78,13 @@ WSGI_APPLICATION = 'LinkWolf.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'onlyhub',
-        'USER':'root',
-        'PASSWORD':'1kUn_D[LiKInm[pF',
-        'HOST':'localhost',
-        'PORT':'3306'  
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
