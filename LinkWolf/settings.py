8e115d0195f428b3a02a566c42434adf6318680d
Elias Eduardo Liranza Pérez <146482192+eliaselp@users.noreply.github.com>
2024-04-01 08:01:38 -0700

    Add files via upload

---
+++ LinkWolf/settings.py
@@ -0,0 +1,172 @@
+from pathlib import Path
+import os
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-bl4&rg5q6tid^zylt%i3%i(n0i!-wl+0@q0l0pr#5()-_i6!m2'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'jazzmin',
+    'channels',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'App',
+]
+
+
+
+ASGI_APPLICATION = 'LinkWolf.asgi.application'
+
+SESSION_COOKIE_AGE = 3600
+ENVIO_EMAIL=False
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'LinkWolf.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+
+WSGI_APPLICATION = 'LinkWolf.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'onlyhub',
+        'USER':'root',
+        'PASSWORD':'1kUn_D[LiKInm[pF',
+        'HOST':'localhost',
+        'PORT':'3306'  
+    }
+}
+
+# Password validation
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.2/topics/i18n/
+
+LANGUAGE_CODE = 'es-eu'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = '/static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_USE_TLS=True
+EMAIL_PORT=587
+EMAIL_HOST_USER="linkwolf.cuba@gmail.com"
+EMAIL_HOST_PASSWORD="rdswkavmfiqxlscn"
+
+JAZZMIN_SETTINGS = {
+    # Título de la ventana (predeterminado a current_admin_site.site_title si está ausente o None)
+    "site_title": "Admin OnlyHub",
+
+    # Título en la pantalla de inicio de sesión (máximo 19 caracteres, predeterminado a current_admin_site.site_header si está ausente o None)
+    "site_header": "OnlyHub",
+
+    # Título en la marca (máximo 19 caracteres, predeterminado a current_admin_site.site_header si está ausente o None)
+    "site_brand": "OnlyHub Admin",
+
+    "site_logo": "C:/Users/KHR/Desktop/django-LinkWolf/LinkWolf/static/img/logo2.png",
+    "login_logo": "C:/Users/KHR/Desktop/django-LinkWolf/LinkWolf/static/img/logo.png",
+    "custom_css":"C:/Users/KHR/Desktop/django-LinkWolf/LinkWolf/static/stiles/admin.css",
+}
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
+LOGIN_URL='App.views.login'
+
